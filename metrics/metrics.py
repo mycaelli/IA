40af67ddd8173f71de5fdee665bc210fd82ed3ff
mycaelli.lima <mycaelli.lima@g.globo>
2024-05-28 19:54:34 -0300

    wip

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,32 +1,29 @@
-import ia.matrix as matrix 
+import ia.matrix as matrix
 import numpy as np
 # tem que adicionar informacoes pra conseguir reproduzir o resultado depois
 
 def calculate_metrics(output_prediction, expected_output):
+  TP, FP, TN, FN = 0, 0, 0, 0
 
-  loss = loss = matrix.sum(output_prediction**2)
-  # print('Predction, expected', output_prediction, expected_output)
-  true_positives = np.sum((output_prediction == 1) & (expected_output == 1))
-  false_positives = np.sum((output_prediction == 1) & (expected_output == 0))
-  true_negatives = np.sum((output_prediction == 0) & (expected_output == 0))
-  false_negatives = np.sum((output_prediction == 0) & (expected_output == 1))
+  for pred, exp in zip(output_prediction, expected_output):
+    if pred == 1 and exp == 1:
+        TP += 1
+    elif pred == 1 and exp == -1:
+        FP += 1
+    elif pred == -1 and exp == -1:
+        TN += 1
+    elif pred == -1 and exp == 1:
+        FN += 1
 
-  # print(true_positives)
-  # print(false_positives)
-  # print(true_negatives)
-  # print(false_negatives)
-
-  accuracy = (true_positives + true_negatives) / len(expected_output) # acuracia
-
-  if true_positives + false_positives != 0:
-    precision = true_positives / (true_positives + false_positives) # precisao
-  else:
-    precision = 0.0
-
-  if true_positives + false_negatives != 0:
-    recall = true_positives / (true_positives + false_negatives) # sensibilidade
-  else:
-    recall = 0.0
-  # specificity = true_negatives / (false_positives + true_negatives) # especificidade
+  P = TP + FP
+  N = TN + FN
   
-  return loss, accuracy, precision, recall
+  error = 0 # FP + FN / (P + N)
+        
+  accuracy = 0 # TP + TN / (P + N)
+
+  precision = TP/(TP + FP)
+
+  recall = TP / (TP + FN)
+
+  return error, accuracy, precision, recall
